4859f4ce263ba95a727a16a713b59400c8045012
Will Townes <will.townes@gmail.com>
2017-12-04 10:02:56 -0500

    Revert "changing parameterization for stan data"
    This reverts commit e114f4aee1dc6c7c650e683e0f6635e012b986be.

--- 02_stan_fit.R
+++ 02_stan_fit.R
@@ -6,18 +6,6 @@ options(mc.cores=4)
 
 dat<-read.csv("data/merged_data.csv")
 
-#regression models are as follows (mu=mean abundance)
-#site BL has two species: blnh and rbnh. We code blnh to "endm" for endemic
-#site PO has two species: ponh and rbnh. We code ponh to "endm" for endemic
-#we analyze each site separately as follows:
-#log(mu)= -a_j*(temp-offset)^2 + b_j*(temp-offset) + c_j + d[randint_date] + w*SoilWC
-#regression parameters a_j,b_j,c_j for j=1 (endemic) and j=2 (rbnh) describe a quadratic temperature curve specific to each species
-#we want the parabola to be concave down, so we want a_j<0. Use a Gamma(2,1) prior for -a_j
-#use weak Cauchy(0,5) prior for b_j. Use improper uniform prior for intercepts c_j
-#parameters d (random intercepts by date) and w (linear effect of soil moisture) are not species-specific.
-#data X,y split into X1,y1 (observations with saltype=endemic) and X2,y2 (saltype=rbnh)
-#columns of X are (Intercept),soilWC,temp,temp2, created from model.matrix on subset data.
-
 dat2stan<-function(d,site=c("BL","PO"),temp_offset=10){
   #package data into list
   site<-match.arg(site)
@@ -26,8 +14,7 @@ dat2stan<-function(d,site=c("BL","PO"),temp_offset=10){
   d2$temp2<-d2$temp^2
   soilWCmedian<-median(d2$soilWC,na.rm = TRUE)
   d2$soilWC<-d2$soilWC - soilWCmedian
-  endm<-if(site=="BL") "blnh" else "ponh"
-  d2$saltype<-plyr::mapvalues(d2$saltype, from=endm, to="endm")
+  d2$saltype<-plyr::revalue(d2$saltype, c("ponh"="endm"))
   dat<-list(id=as.integer(as.factor(d2$Date)),temp_offset=temp_offset,soilWCmedian=soilWCmedian)
   dat$K<-max(dat$id) #number of clusters
   dat$X<-model.matrix(~soilWC+saltype*(temp+temp2),d2) #fixed covariates
